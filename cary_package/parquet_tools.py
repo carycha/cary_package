ecf7b062249eecdf8ddc7d75923bed9224314263
carycha <carycha@gmail.com>
2025-02-05 10:31:42 +0000

    adjust return

--- cary_package/parquet_tools.py
+++ cary_package/parquet_tools.py
@@ -3,36 +3,37 @@ import polars as pl
 import pandas as pd
 
 
-def load_pq(file_path):
+def load_pq(file_path, return_df=False):
     # df = pd.read_parquet(file_path, engine='pyarrow')
 
     # # 转换为列表字典格式
     # data = df.to_dict(orient='records')
+
     """
-    Load data from a Parquet file and return it as a list of dictionaries.
+    Load a Parquet file.
 
     Parameters
     ----------
     file_path : str
         The path to the Parquet file to be loaded.
+    return_df : bool, optional
+        If True, return a polars DataFrame instead of a list of dictionaries.
 
     Returns
     -------
-    list
-        A list of dictionaries representing the data loaded from the Parquet file.
-
-    Raises
-    ------
-    Exception
-        If there is an error loading the Parquet file.
+    data : list or polars.DataFrame
+        A list of dictionaries or a polars DataFrame loaded from the Parquet file.
     """
-
     try:
         df = pl.read_parquet(file_path)
-        data = df.to_dicts()
     except Exception as e:
         raise Exception(f"load_pq 失敗: {e}")
-    return data
+
+    if return_df:
+        return df
+    else:
+        data = df.to_dicts()
+        return data
 
 
 def save_pq(file_path, data_list):
